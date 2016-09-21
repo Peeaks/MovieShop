2864c49b4cb067692417c2a7339869ff1cd9d291
Rasmusmm96 <rallemm96@gmail.com>
2016-09-21 10:28:18 +0200

    Implermenterede AddressManager

--- DLL/Managers/AddressManager.cs
+++ DLL/Managers/AddressManager.cs
@@ -1,27 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using DLL.Contexts;
 using DLL.Entities;
 
 namespace DLL {
     internal class AddressManager : IManager<Address> {
         public Address Create(Address element) {
-            throw new NotImplementedException();
+            using (var db = new MovieShopContext()) {
+                db.Addresses.Add(element);
+                db.SaveChanges();
+                return element;
+            }
         }
 
         public bool Delete(int id) {
-            throw new NotImplementedException();
+            using (var db = new MovieShopContext()) {
+                db.Entry(db.Addresses.FirstOrDefault(x => x.Id == id)).State = System.Data.Entity.EntityState.Deleted;
+                db.SaveChanges();
+                return db.Addresses.FirstOrDefault(x => x.Id == id) == null;
+            }
         }
 
         public List<Address> Read() {
-            throw new NotImplementedException();
+            using (var db = new MovieShopContext()) {
+                return db.Addresses.ToList();
+            }
         }
 
         public Address Read(int id) {
-            throw new NotImplementedException();
+            using (var db = new MovieShopContext()) {
+                return db.Addresses.FirstOrDefault(x => x.Id == id);
+            }
         }
 
         public Address Update(Address element) {
-            throw new NotImplementedException();
+            using (var db = new MovieShopContext()) {
+                db.Entry(element).State = EntityState.Modified;
+                db.SaveChanges();
+                return element;
+            }
         }
     }
 }
