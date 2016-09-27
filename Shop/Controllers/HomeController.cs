a4531bfd1fc79dc353954d53433fa3b6ad80e559
Peeaks <Kristoffer2509@icloud.com>
2016-09-27 08:25:35 +0200

    Added another MVC project

---
+++ Shop/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DLL;
+using DLL.Entities;
+
+namespace Shop.Controllers {
+    public class HomeController : Controller {
+
+        private readonly IManager<Movie> _movieManager = new DllFacade().GetMovieManager();
+
+        public ActionResult Index() {
+            return View(_movieManager.Read());
+        }
+
+        public ActionResult About() {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact() {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
