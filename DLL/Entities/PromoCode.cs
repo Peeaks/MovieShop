4a298e071f81fe7c10c9947e1dbe4fd447d54e70
Rasmusmm96 <rallemm96@gmail.com>
2016-10-04 13:57:31 +0200

    Promo

---
+++ DLL/Entities/PromoCode.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLL.Entities {
+    class PromoCode : AbstractEntity{
+        public string Code { get; set; }
+        public int Discount { get; set; }
+    }
+}
